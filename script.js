d17f413986ac00ff710e7403165836085bfff488
roberttsouza <roberttsouzzaa@gmail.com>
2022-10-28 19:45:14 -0300

    :bricks: criado a estrutura do HTML e feito o script do Timer

---
+++ script.js
@@ -0,0 +1,46 @@
+//setando as variaveis do relogio
+
+let sec = 00;
+let min = 00;
+let hr = 00;
+const watch = document.querySelector("#watch"); 
+
+//função para adicionar um zero a frente dos variaveis
+function twoDigits(digits){
+  if(digits<10){
+    return('0'+digits)
+  }else{
+    return(digits)
+  }
+}
+
+//arrowfunction para acrescentar um segundo ao relogio
+start = () => {
+  interval = setInterval(conter,1)
+}
+
+//função para parar o relogio
+pause = () => {
+  clearInterval(interval)
+}
+//função para zerar o relogio
+stop = () => {
+  clearInterval(interval)
+  min=0
+  sec=0
+  watch.innerHTML = `${twoDigits(hr)}:${twoDigits(min)}:${twoDigits(sec++)}`
+}
+
+
+//função que aprsenta na tela o timer e 
+function conter(){
+  if(sec==60){
+    min++
+    sec=0
+  }if(min==60){
+    hr++
+    min=0
+    sec=0
+  }
+  watch.innerHTML = `${twoDigits(hr)}:${twoDigits(min)}:${twoDigits(sec++)}`;
+}
